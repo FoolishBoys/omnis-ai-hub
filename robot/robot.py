6402c02c960792cfe2e500965ec6dde8a9d9d928
C-Home <1391023795@qq.com>
2025-03-24 23:13:48 +0800

    coze测试

--- robot/robot.py
+++ robot/robot.py
@@ -1,27 +1,28 @@
 # -*- coding: utf-8 -*-
 
+from config.configuration import Config
 import logging
 import re
 import time
 import xml.etree.ElementTree as ET
 from queue import Empty
 from threading import Thread
-from base.func_zhipu import ZhiPu
 
 from wcferry import Wcf, WxMsg
-
-from base.func_bard import BardAssistant
-from base.func_chatglm import ChatGLM
-from base.func_ollama import Ollama
-from base.func_chatgpt import ChatGPT
+from base.func_coze_test import CozeTest
 from base.func_chengyu import cy
 from base.func_weather import Weather
 from base.func_news import News
-from base.func_tigerbot import TigerBot
-from base.func_xinghuo_web import XinghuoWeb
-from config.configuration import Config
-from config.constants import ChatType
+
 from job_mgmt import Job
+
+# from base.old_func.func_bard import BardAssistant
+# from base.old_func.func_chatglm import ChatGLM
+# from base.old_func.func_ollama import Ollama
+# from base.old_func.func_tigerbot import TigerBot
+# from base.old_func.func_xinghuo_web import XinghuoWeb
+# from config.constants import ChatType
+# from base.old_func.func_zhipu import ZhiPu
 
 __version__ = "39.2.4.0"
 
@@ -38,42 +39,44 @@ class Robot(Job):
         self.allContacts = self.getAllContacts()
         self._msg_timestamps = []
 
-        if ChatType.is_in_chat_types(chat_type):
-            if chat_type == ChatType.TIGER_BOT.value and TigerBot.value_check(self.config.TIGERBOT):
-                self.chat = TigerBot(self.config.TIGERBOT)
-            elif chat_type == ChatType.CHATGPT.value and ChatGPT.value_check(self.config.CHATGPT):
-                self.chat = ChatGPT(self.config.CHATGPT)
-            elif chat_type == ChatType.XINGHUO_WEB.value and XinghuoWeb.value_check(self.config.XINGHUO_WEB):
-                self.chat = XinghuoWeb(self.config.XINGHUO_WEB)
-            elif chat_type == ChatType.CHATGLM.value and ChatGLM.value_check(self.config.CHATGLM):
-                self.chat = ChatGLM(self.config.CHATGLM)
-            elif chat_type == ChatType.BardAssistant.value and BardAssistant.value_check(self.config.BardAssistant):
-                self.chat = BardAssistant(self.config.BardAssistant)
-            elif chat_type == ChatType.ZhiPu.value and ZhiPu.value_check(self.config.ZhiPu):
-                self.chat = ZhiPu(self.config.ZhiPu)
-            elif chat_type == ChatType.OLLAMA.value and Ollama.value_check(self.config.OLLAMA):
-                self.chat = Ollama(self.config.OLLAMA)
-            else:
-                self.LOG.warning("未配置模型")
-                self.chat = None
-        else:
-            if TigerBot.value_check(self.config.TIGERBOT):
-                self.chat = TigerBot(self.config.TIGERBOT)
-            elif ChatGPT.value_check(self.config.CHATGPT):
-                self.chat = ChatGPT(self.config.CHATGPT)
-            elif Ollama.value_check(self.config.OLLAMA):
-                self.chat = Ollama(self.config.OLLAMA)
-            elif XinghuoWeb.value_check(self.config.XINGHUO_WEB):
-                self.chat = XinghuoWeb(self.config.XINGHUO_WEB)
-            elif ChatGLM.value_check(self.config.CHATGLM):
-                self.chat = ChatGLM(self.config.CHATGLM)
-            elif BardAssistant.value_check(self.config.BardAssistant):
-                self.chat = BardAssistant(self.config.BardAssistant)
-            elif ZhiPu.value_check(self.config.ZhiPu):
-                self.chat = ZhiPu(self.config.ZhiPu)
-            else:
-                self.LOG.warning("未配置模型")
-                self.chat = None
+        self.chat = CozeTest(self.config.CHATGPT)
+        
+        # if ChatType.is_in_chat_types(chat_type):
+        #     if chat_type == ChatType.TIGER_BOT.value and TigerBot.value_check(self.config.TIGERBOT):
+        #         self.chat = TigerBot(self.config.TIGERBOT)
+        #     elif chat_type == ChatType.CHATGPT.value and ChatGPT.value_check(self.config.CHATGPT):
+        #         self.chat = ChatGPT(self.config.CHATGPT)
+        #     elif chat_type == ChatType.XINGHUO_WEB.value and XinghuoWeb.value_check(self.config.XINGHUO_WEB):
+        #         self.chat = XinghuoWeb(self.config.XINGHUO_WEB)
+        #     elif chat_type == ChatType.CHATGLM.value and ChatGLM.value_check(self.config.CHATGLM):
+        #         self.chat = ChatGLM(self.config.CHATGLM)
+        #     elif chat_type == ChatType.BardAssistant.value and BardAssistant.value_check(self.config.BardAssistant):
+        #         self.chat = BardAssistant(self.config.BardAssistant)
+        #     elif chat_type == ChatType.ZhiPu.value and ZhiPu.value_check(self.config.ZhiPu):
+        #         self.chat = ZhiPu(self.config.ZhiPu)
+        #     elif chat_type == ChatType.OLLAMA.value and Ollama.value_check(self.config.OLLAMA):
+        #         self.chat = Ollama(self.config.OLLAMA)
+        #     else:
+        #         self.LOG.warning("未配置模型")
+        #         self.chat = None
+        # else:
+        #     if TigerBot.value_check(self.config.TIGERBOT):
+        #         self.chat = TigerBot(self.config.TIGERBOT)
+        #     elif ChatGPT.value_check(self.config.CHATGPT):
+        #         self.chat = ChatGPT(self.config.CHATGPT)
+        #     elif Ollama.value_check(self.config.OLLAMA):
+        #         self.chat = Ollama(self.config.OLLAMA)
+        #     elif XinghuoWeb.value_check(self.config.XINGHUO_WEB):
+        #         self.chat = XinghuoWeb(self.config.XINGHUO_WEB)
+        #     elif ChatGLM.value_check(self.config.CHATGLM):
+        #         self.chat = ChatGLM(self.config.CHATGLM)
+        #     elif BardAssistant.value_check(self.config.BardAssistant):
+        #         self.chat = BardAssistant(self.config.BardAssistant)
+        #     elif ZhiPu.value_check(self.config.ZhiPu):
+        #         self.chat = ZhiPu(self.config.ZhiPu)
+        #     else:
+        #         self.LOG.warning("未配置模型")
+        #         self.chat = None
 
         self.LOG.info(f"已选择: {self.chat}")
 
